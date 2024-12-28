4320dc3114a5b11b403f3edfbcc344030daa9fb8
Mohammad Seyam <mas_seyam@yahoo.com>
2024-12-28 02:57:05 +0200

    intialize project, app and DB

---
+++ database/db.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+exports.connectMongoDB = async function () {
+  const DB = `${process.env.DATABASE}${process.env.DATABASE_NAME}`;
+  await mongoose.connect(DB);
+};
