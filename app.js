e3058133b51783ffcb719238e3037aed61d6a1c7
Mohammad Seyam <mas_seyam@yahoo.com>
2024-12-29 04:38:52 +0200

    'initial commit'

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ const helmet = require('helmet');
 const compression = require('compression');
 const mongoSanitize = require('express-mongo-sanitize');
 const morgan = require('morgan');
+const authRouter = require('./routes/Auth/router');
+const errorController = require('./controllers/errorController');
+
 const app = express();
+
 app.use(helmet());
+
 app.use(
   express.json({
     limit: '50mb',
   })
 );
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(compression());
 app.use(mongoSanitize());
+
+const mainRouter = express.Router();
+app.use('/api/v1', mainRouter);
+// After finding /api/v1
+mainRouter.use('/auth', authRouter);
+app.use('*', (req, res, next) => {
+  return res.status(404).json({
+    message: 'API is not found',
+  });
+});
+app.use(errorController);
 module.exports = app;
---
+++ routes/Auth/router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const userRouter = require('./userRouter');
+const authRouter = express.Router();
+authRouter.use('/user', userRouter);
+module.exports = authRouter;
